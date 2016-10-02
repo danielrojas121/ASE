68b943cb484165f4ee32d2f3929c4d81d4f5a42e
frankcabada <frankcabada@yahoo.com>
2016-10-01 20:24:34 -0400

    Initialized MySQL database connection in app.py

--- HW2-FrankDuru/app.py
+++ HW2-FrankDuru/app.py
@@ -1,11 +1,27 @@
 from flask import Flask, request, render_template
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 app.config["DEBUG"] = True  # Only include this while you are testing your app
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'frankcabada'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Fc182641!'
+app.config['MYSQL_DATABASE_DB'] = 'Vanmo'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route("/")
 def home():
-	return render_template("home.html")
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.execute("SELECT VERSION()")
+	data = cursor.fetchone()
+	conn.close()
+	return "Database version : %s " % data
+	#return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
+
