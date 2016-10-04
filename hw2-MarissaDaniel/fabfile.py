02952a9b8199017c117716583fe8c752883a547c
Daniel Rojas <danielrojas@dyn-160-39-208-84.dyn.columbia.edu>
2016-10-03 22:46:09 -0400

    Added separate functions in fabfile for init and running server

--- hw2-MarissaDaniel/fabfile.py
+++ hw2-MarissaDaniel/fabfile.py
@@ -1,9 +1,12 @@
 from fabric.api import local
 
-def run_server():
+def static_analyzer():
+	local("pylint server.py")
+
+def init_server():
 	local("export FLASK_APP=server.py")
 	local("flask initdb")
 	local("flask run")
 
-def static_analyzer():
-	local("pylint server.py")
+def run_server():
+	local("flask run")
