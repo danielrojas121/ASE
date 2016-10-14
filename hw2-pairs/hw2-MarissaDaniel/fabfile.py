bbace2977d8fa7ca2be6f2d9e56282105cf0e58a
Daniel Rojas <danielrojas@dyn-160-39-208-20.dyn.columbia.edu>
2016-10-14 16:18:15 -0400

    Modified hw2 fabfile

--- hw2-pairs/hw2-MarissaDaniel/fabfile.py
+++ hw2-pairs/hw2-MarissaDaniel/fabfile.py
@@ -1,11 +1,26 @@
 from fabric.api import local
+from fabric.utils import abort
+import os.path
+
+html_files = ["home.html", "signup.html"]
 
 def static_analyzer():
+	'''Runs Pylint analyzer'''
 	local("pylint server.py")
 
 def init_server():
+	'''Initializes the database'''
 	local("export FLASK_APP=server.py")
 	local("flask initdb")
 
 def run_server():
+	'''Runs the application'''
+	verify_files()
 	local("flask run")
+
+def verify_files():
+    '''test that all html files used in app.py exist in templates folder'''
+    for file in html_files:
+    	file_path = "templates/" + file
+        if (not os.path.isfile(file_path)):
+    	    abort("No file %s found in /templates/" % file)
