c555d20e1cbfb6d0d8593ae27faeaaf94238c32f
frankcabada <frankcabada@yahoo.com>
2016-10-19 16:02:45 -0400

    Added static analyzer and initialization functionality to fabfile, fixed fab complaints for app.py

--- app.py
+++ app.py
@@ -7,52 +7,52 @@ import os
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, render_template, request, g, redirect
 
-app = Flask(__name__)
-app.config.from_object(__name__)
-app.config["DEBUG"] = True  # Only  include this while you are testing your app
+APP = Flask(__name__)
+APP.config.from_object(__name__)
+APP.config["DEBUG"] = True  # Only  include this while you are testing your APP
 
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'vanmo.db'),
+APP.config.update(dict(
+    DATABASE=os.path.join(APP.root_path, 'vanmo.sql_db'),
     SECRET_KEY='development key',
     USERNAME='admin',
     PASSWORD='default'
 ))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+APP.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-@app.route("/")
+@APP.route("/")
 def home():
     """Home page showing usernames and passwords."""
-    db = get_db()
-    cur = db.execute('select username, password from logins order by id asc')
+    sql_db = get_db()
+    cur = sql_db.execute('select username, password from logins order by id asc')
     logins = cur.fetchall()
     return render_template("home.html", logins=logins)
 
-@app.route("/signup", methods=["POST", "GET"])
+@APP.route("/signup", methods=["POST", "GET"])
 def signup():
     """Signup page where people can add username and password."""
     if request.method == "POST":
-        db = get_db()
-        db.execute('insert into logins (username, password) values (?, ?)',
-                   [request.form['username'], request.form['password']])
-        db.commit()
+        sql_db = get_db()
+        sql_db.execute('insert into logins (username, password) values (?, ?)',
+                       [request.form['username'], request.form['password']])
+        sql_db.commit()
         return redirect("/")
     else:
         return render_template("signup.html")
 
 def connect_db():
     """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
+    db_rv = sqlite3.connect(APP.config['DATABASE'])
+    db_rv.row_factory = sqlite3.Row
+    return db_rv
 
 def init_db():
-    """Initializes db"""
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
+    """Initializes sql_db"""
+    sql_db = get_db()
+    with APP.open_resource('schema.sql', mode='r') as db_file:
+        sql_db.cursor().executescript(db_file.read())
+    sql_db.commit()
 
-@app.cli.command('initdb')
+@APP.cli.command('initdb')
 def initdb_command():
     """Initializes the database."""
     init_db()
@@ -60,17 +60,19 @@ def initdb_command():
 
 def get_db():
     """Opens a new database connection if there is none yet for the
-    current application context.
+    current APPlication context.
     """
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
-@app.teardown_appcontext
+@APP.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+    else:
+        print error
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    APP.run(host="0.0.0.0")
--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,26 @@
 from fabric.api import local
+import platform
+
+def prepare_deploy():
+	static_analyzer()
+	init_server()
+	run_server()
 
 def static_analyzer():
 	local("pylint app.py")
 
 def init_server():
-	local("export FLASK_APP=app.py")
+	if (platform.system() == 'Windows'):
+		windows_init()
+	else:
+		default_init()
 	local("flask initdb")
+
+def default_init():
+	local("export FLASK_APP=app.py")
+
+def windows_init():
+	local("set FLASK_APP=app.py")
 
 def run_server():
 	local("flask run")
