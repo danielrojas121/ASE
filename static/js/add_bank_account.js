7e279aed42be64f129def0e57fdfb06b96c4ee81
frankcabada <frankcabada@yahoo.com>
2016-10-28 13:26:52 -0400

    Fixed savings account bug

--- static/js/add_bank_account.js
+++ static/js/add_bank_account.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
   $(".dropdown-menu li a").click(function(){
     $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
     $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
-    $(this).parents(".dropdown").find('#account_type').val($(this).data('value'));
+    $(this).parents("#new-account-form").find('#account_type').val($(this).data('value'));
   });
 });
