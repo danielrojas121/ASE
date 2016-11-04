5eddc44d0a34cf7f9f7ba61e84a19042fbe2dade
Daniel Rojas <danielrojas@dyn-160-39-209-111.dyn.columbia.edu>
2016-11-04 16:37:54 -0400

    Created getters and setters in account.py

--- account.py
+++ account.py
@@ -2,8 +2,27 @@
 
 class Account(object):
 
-    def __init__(self, account_name, username, account_type, balance):
-        self.account_name = account_name
-        self.username = username
-        self.account_type = account_type
-        self.balance = balance
+    def __init__(self, account_id, account_name, username, account_type, balance):
+        self.__account_id = account_id
+        self.__account_name = account_name
+        self.__username = username
+        self.__account_type = account_type
+        self.__balance = balance
+
+        def getAccountId(self):
+        	return self.__account_id
+
+        def getAccountName(self):
+        	return self.__account_name
+
+        def getUsername(self):
+        	return self.__username
+
+        def getAccountType(self):
+        	return self.__account_type
+
+        def getBalance(self):
+        	return self.__balance
+
+        def setBalance(self, newBalance):
+        	self.__balance = newBalance
