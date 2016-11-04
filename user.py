af417c43ac24b259d7fec2683360b3f7b08af90f
frankcabada <frankcabada@yahoo.com>
2016-11-04 16:37:59 -0400

    Created getters and setters for user class

--- user.py
+++ user.py
@@ -1,7 +1,22 @@
 '''User Class for Vanmo app'''
+from account import Account
 
 class User(object):
 
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+    def __init__(self, user_id, username, password):
+        self.__user_id = user_id
+        self.__username = username
+        self.__password = password
+        self.__accounts = [] #list to keep track of this user's accounts
+
+    def getUserId(self):
+        return self.__user_id
+
+    def getUserName(self):
+        return self.__username
+
+    def getAccounts(self):
+        return self.__acounts
+
+    def addAccount(self, account_object):
+        self.__accounts.append(account_object)
