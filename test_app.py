96777e22d4a2d862db5efe4066e6f10db8c38173
Daniel Rojas <danielrojas@dyn-160-39-208-113.dyn.columbia.edu>
2016-11-09 22:01:07 -0500

    Added signup tests, added entries to login db table and checked it

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 # content of test_sample.py
 import pytest
 
-'''Test various get methods on app routes'''
+'''Various test get methods on app routes'''
 def test_get_home_OK(client):
 	assert client.get('/').status_code == 200
 
@@ -14,21 +14,18 @@ def test_get_view_account_redirect(client):
 def test_get_add_account_redirect(client):
 	assert client.get('/add_bank_account').status_code == 302
 
-
-
 '''Login post tests'''
 def test_post_login_bad_request(client):
 	assert client.post('/login').status_code == 400
 
 def test_bad_login(client):
-	assert client.post('/login', data=dict(usr = 'nelly', pwd = 'dog')).status_code == 302
+	assert client.post('/login', data=dict(usr = 'martychavezgarzamcfly', pwd = 'dog')).status_code == 302
 	assert client.get('/view_current_account').status_code == 302
 
 def test_good_login(client):
-	assert client.post('/login', data=dict(usr = 'daniel', pwd = 'hi')).status_code == 302
+	assert client.post('/signup', data=dict(username = 'testinglogin', password = 'test')).status_code == 302
+	assert client.post('/login', data=dict(usr = 'testinglogin', pwd = 'test')).status_code == 302
 	assert client.get('/view_current_account').status_code == 200
-
-
 
 '''Login empty fields tests'''
 def test_login_empty_usr(client):
@@ -43,8 +40,13 @@ def test_login_empty(client):
 	assert client.post('/login', data=dict(usr = '', pwd = '')).status_code == 302
 	assert client.get('/view_current_account').status_code == 302
 
-
 '''Signup empty bad request test'''
 def test_signup_empty_bad_request(client):
 	assert client.post('/signup', data=dict(usr = '', pwd = '')).status_code == 400
 	assert client.get('/view_current_account').status_code == 302
+
+def test_signup_empty_usr(client):
+	assert client.post('/signup', data=dict(username = '', password = 'test')).status_code == 302
+
+def test_signup_empty_pass(client):
+	assert client.post('/signup', data=dict(username = 'Michael', password = '')).status_code == 302
